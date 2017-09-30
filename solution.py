511004aaa722e777ada64a4e3d981db221effd5b
Tim <tmpa4844@gmail.com>
2017-09-30 11:02:08 -0400

    Update and rename solution to solution.py

--- solution.py
+++ solution.py
@@ -1,3 +1,3 @@
-def problem1():
+def problem_one():
     """Add all the natural numbers below 1000 that are multiples of 3 or 5.""" 
     print sum(x for x in xrange(1, 1000) if x % 3 == 0 or x % 5 == 0)
